88191114b0dcc917af9575586acb0c7a97536c7e
tommycck <91048428+tommycck@users.noreply.github.com>
2023-07-25 12:21:12 +0800

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by tommy chow on 25/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by tommy chow on 25/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
